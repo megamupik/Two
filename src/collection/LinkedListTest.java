73247cdaf382337c101e84a944ebddaec8a875ae
megamupik <megamupik@yandex.ru>
2018-10-05 15:27:29 +0300

    !Two

--- src/collection/LinkedListTest.java
+++ src/collection/LinkedListTest.java
@@ -2,6 +2,7 @@ package collection;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class LinkedListTest {
     public static void main(String[] args) {
@@ -10,14 +11,17 @@ public class LinkedListTest {
         staff.add("Amy");
         staff.add("Bob");
         staff.add("Carl");
-        Iterator iter = staff.iterator();
+        ListIterator<String> iter = staff.listIterator();
+        iter.next(); // Пройти первый элемент списка
+        iter.add("Juliet");
+
         String first = (String) iter.next(); // Обратиться к первому элементу
-        Object second = iter.next(); // Обратиться ко второму элементу
+//        Object second = iter.next(); // Обратиться ко второму элементу
+        String second = String.valueOf(iter.next()); // Обратиться к третьему элементу
+//        iter.remove();
 //        String three = String.valueOf(iter.next()); // Обратиться к третьему элементу
-        iter.remove();
-        String three = String.valueOf(iter.next()); // Обратиться к третьему элементу
         System.out.println(first);
         System.out.println(second);
-        System.out.println(three);
+//        System.out.println(three);
     }
 }
